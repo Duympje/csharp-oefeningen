c376416817a3c200c633309a91100f1f8239a6ae
Jorne Duym <jorne.duym@icloud.com>
2025-06-14 15:42:31 +0200

    Add project files.

---
+++ Persoonsregistratie/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Persoonsregistratie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
